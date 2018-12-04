6c6ac4496395c73f1ecf0b219ec2903e5e12dab4
celkou <koushiroue@gmail.com>
2018-12-04 19:42:50 +0800

    fix padding + nyako script

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,11 @@
-/* PLS SEND HELP IDK HOW TO JAVASCRIPT*/
+// PLS SEND HELP IDK HOW TO JAVASCRIPT//
+
+var nyako   //This is Nyako
+
+nyako = "Hewwo UWU Uworwd"  //This is Nyako saying hello wolrd in it's local languange.
+
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
 
